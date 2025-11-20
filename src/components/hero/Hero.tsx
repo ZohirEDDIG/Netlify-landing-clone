67bb3c2dc04f055d2e61411ff58557e02bee533c
ZohirEDDIG <zohireddig.dev@gmail.com>
2025-11-20 10:19:23 +0100

    Update Hero Responsive

--- src/components/hero/Hero.tsx
+++ src/components/hero/Hero.tsx
@@ -4,7 +4,7 @@ import HeroImage from './subcomponents/HeroImage';
 
 const Hero = () => {
     return (
-        <section className='h-screen-minus-header'>
+        <section className='h-[600px]'>
 
             {/* Top */}
             <div className='ctn h-full flex flex-col md:flex-row justify-center md:justify-between items-center gap-10'>
@@ -13,7 +13,7 @@ const Hero = () => {
                 <div className='flex flex-col gap-y-6'>
 
                     {/* Hero title */}
-                    <h1 className='text-white text-4xl md:text-5xl elg:text-6xl  font-bold'>Push your ideas <br /> to the web</h1>
+                    <h1 className='text-white text-4xl md:text-5xl elg:text-6xl font-bold'>Push your ideas <br /> to the web</h1>
 
                     {/* Hero text */}
                     <p className='text-light-green-quaternary text-sm sm:text-base elg:text-lg max-w-[500px]'>
